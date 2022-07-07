ffdc01467345e776903515082ff35dd212f0da1d
Ninette Calderon <ninettecalderon@Ninettes-MacBook-Pro-4.local>
2022-07-07 14:05:56 -0700

    finale

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -3,7 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import "./singlePost.css";
-import {axiosInstance} from "../../config";
+import { axiosInstance } from "../../config";
 
 export default function SinglePost() {
   const location = useLocation();
@@ -33,7 +33,7 @@ export default function SinglePost() {
         data: { username: user.username },
       })
       window.location.replace("/");
-    } catch (err) {}
+    } catch (err) { }
   };
 
   const handleUpdate = async () => {
@@ -45,30 +45,37 @@ export default function SinglePost() {
         desc,
       });
       setUpdateMode(false)
-    } catch (err) {}
+    } catch (err) { }
   };
 
   return (
     <div className="singlePost">
-            <div className="singlePostWrapper">
-                {post.photo && <img src={PF + post.photo} alt="" className="singlePostImg" />}
-                {updateMode ? <input type="text" value={title} className="singlePostTitleInput" autoFocus onChange={(e)=>{setTitle(e.target.value)}}></input> : <h1 className="singlePostTitle">
-                    {title}
-                    {post.username === user?.username && (<div className="singlePostEdit">
-                        <i className="singlePostIcon fa-solid fa-pen-to-square" onClick={()=>{setUpdateMode(true)}}>Update this post</i>
-                        <i className="singlePostIcon fa-solid fa-trash-can" onClick={ handleDelete }> Delete</i>
-                    </div>)}
-                </h1>}
-                <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <Link to={`/?user=${post.username}`} className="link"><b>{post.username}</b></Link></span>
-                    <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
-                </div>
-                {updateMode ? <textarea className="singlePostDescInput" value={desc} onChange={(e)=>{setDesc(e.target.value)}}/> : <p className="singlePostDesc">
-                    {desc}
-                </p>}
-                {updateMode && <button className="singlePostButton" onClick={handleUpdate}>Update</button>}
-                
-            </div>
+      <div className="singlePostWrapper">
+        {post.photo && <img src={PF + post.photo} alt="" className="singlePostImg" />}
+        {updateMode ? <input type="text" value={title} className="singlePostTitleInput" autoFocus onChange={(e) => { setTitle(e.target.value) }}></input> : 
+        <h1 className="singlePostTitle">
+          {title}
+          {post.username === user?.username && 
+          (<div className="singlePostEdit">
+            <i className="edit" onClick={() => { setUpdateMode(true) }}>
+            Update this post
+            </i>
+            <i className="delete" onClick = {handleDelete}> 
+            Delete
+            </i>
+          </div>
+          )}
+        </h1>}
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">Author: <Link to={`/?user=${post.username}`} className="link"><b>{post.username}</b></Link></span>
+          <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
         </div>
-    )
+        {updateMode ? <textarea className="singlePostDescInput" value={desc} onChange={(e) => { setDesc(e.target.value) }} /> : <p className="singlePostDesc">
+          {desc}
+        </p>}
+        {updateMode && <button className="singlePostButton" onClick={handleUpdate}>Update</button>}
+
+      </div>
+    </div>
+  )
 }
